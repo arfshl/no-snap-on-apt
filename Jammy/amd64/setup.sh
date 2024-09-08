e8bb06bcf52383550d45d43361ca6f7175286af0
Alif <98870352+arfshl@users.noreply.github.com>
2024-09-08 16:28:06 +0700

    Update

---
+++ Jammy/amd64/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+sudo su
+echo 'Adding Linux Mint Repository...'
+sudo wget http://packages.linuxmint.com/pool/main/l/linuxmint-keyring/linuxmint-keyring_2022.06.21_all.deb && sudo dpkg -i linuxmint-keyring_2022.06.21_all.deb && sudo rm linuxmint-keyring_2022.06.21_all.deb
+sudo echo 'deb http://packages.linuxmint.com virginia upstream' >> /etc/apt/sources.list.d/linuxmint.list
+echo 'Configuring APT Pinning...'
+sudo echo 'Package: *
+Pin: origin packages.linuxmint.com
+Pin-Priority: 1
+
+Package: firefox
+Pin: origin packages.linuxmint.com
+Pin-Priority: 1000
+
+Package: chromium
+Pin: origin packages.linuxmint.com
+Pin-Priority: 1000
+
+Package: thunderbird
+Pin: origin packages.linuxmint.com
+Pin-Priority: 1000
+
+Package: firefox
+Pin: release o=Ubuntu
+Pin-Priority: -1
+
+Package: thunderbird
+Pin: release o=Ubuntu
+Pin-Priority: -1
+
+Package: chromium-browser
+Pin: release o=Ubuntu
+Pin-Priority: -1' >> /etc/apt/preferences.d/nativeapt
+sudo apt update
+echo 'Done'

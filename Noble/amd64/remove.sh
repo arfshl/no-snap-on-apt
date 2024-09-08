1b106417ef866bb8f8fbe55d4e66bf212ab53890
Alif <98870352+arfshl@users.noreply.github.com>
2024-09-08 16:27:03 +0700

    Update remove.sh

--- Noble/amd64/remove.sh
+++ Noble/amd64/remove.sh
@@ -3,8 +3,6 @@ sudo su
 echo 'Removing Additional Repository...'
 sudo apt remove linuxmint-keyring_2022.06.21_all.deb
 sudo rm /etc/apt/preferences.d/linuxmint.list
-sudo rm etc/apt/preferences.d/mozilla.list
-sudo rm /etc/apt/keyrings/packages.mozilla.org.asc
 echo 'Removing APT Pinning...'
 sudo rm /etc/apt/preferences.d/nativeapt
 sudo apt update

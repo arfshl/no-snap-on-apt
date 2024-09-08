fe478a9c451ed0b97f3653ff1ed495aaaad4b6ac
Alif <98870352+arfshl@users.noreply.github.com>
2024-09-08 19:33:10 +0700

    Update

---
+++ Focal/amd64/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+sudo su
+echo 'Adding Linux Mint Repository...'
+sudo wget http://packages.linuxmint.com/pool/main/l/linuxmint-keyring/linuxmint-keyring_2022.06.21_all.deb && sudo dpkg -i linuxmint-keyring_2022.06.21_all.deb && sudo rm linuxmint-keyring_2022.06.21_all.deb
+sudo echo 'deb http://packages.linuxmint.com una upstream' >> /etc/apt/sources.list.d/linuxmint.list
+echo 'Configuring APT Pinning...'
+sudo echo 'Package: *
+Pin: origin packages.linuxmint.com
+Pin-Priority: 1
+
+Package: chromium
+Pin: origin packages.linuxmint.com
+Pin-Priority: 1000
+
+Package: chromium-browser
+Pin: release o=Ubuntu
+Pin-Priority: -1' >> /etc/apt/preferences.d/nativeapt
+sudo apt update
+echo 'Done'

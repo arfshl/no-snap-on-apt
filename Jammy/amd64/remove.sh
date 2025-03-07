47faa5af2db9a49cc97ec992a10a82433340a531
arfshl <98870352+arfshl@users.noreply.github.com>
2025-03-07 21:05:26 +0700

    Update remove.sh

--- Jammy/amd64/remove.sh
+++ Jammy/amd64/remove.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-sudo su
 echo 'Removing Additional Repository...'
 sudo apt remove linuxmint-keyring
-sudo rm /etc/apt/preferences.d/linuxmint.list
-sudo rm etc/apt/preferences.d/mozilla.list
+sudo rm /etc/apt/sources.list.d/linuxmint.list
+sudo rm /etc/apt/sources.list.d/mozilla.list
 sudo rm /etc/apt/keyrings/packages.mozilla.org.asc
 echo 'Removing APT Pinning...'
 sudo rm /etc/apt/preferences.d/nativeapt

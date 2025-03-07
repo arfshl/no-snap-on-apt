45ce52be9c4c1a182f5aaa6058020e82756ad9be
arfshl <98870352+arfshl@users.noreply.github.com>
2025-03-07 21:10:12 +0700

    Update remove.sh

--- Jammy/arm64/remove.sh
+++ Jammy/arm64/remove.sh
@@ -2,8 +2,8 @@
 sudo su
 echo 'Removing Additional Repository...'
 sudo apt remove debian-keyring -y
-sudo rm /etc/apt/preferences.d/debian.list
-sudo rm etc/apt/preferences.d/mozilla.list
+sudo rm /etc/apt/sources.list.d/debian.list
+sudo rm /etc/apt/sources.list.d/mozilla.list
 sudo rm /etc/apt/keyrings/packages.mozilla.org.asc
 echo 'Removing APT Pinning...'
 sudo rm /etc/apt/preferences.d/nativeapt

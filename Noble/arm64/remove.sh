e9afc335ff6a91f7e8f75ccd714152d0ebb0d34a
arfshl <98870352+arfshl@users.noreply.github.com>
2025-03-07 21:03:49 +0700

    Update remove.sh

--- Noble/arm64/remove.sh
+++ Noble/arm64/remove.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo 'Removing Additional Repository...'
 sudo apt remove debian-keyring -y
-sudo rm /etc/apt/preferences.d/debian.list
-sudo rm /etc/apt/preferences.d/mozilla.list
+sudo rm /etc/apt/sources.list.d/debian.list
+sudo rm /etc/apt/sources.list.d/mozilla.list
 sudo rm /etc/apt/keyrings/packages.mozilla.org.asc
 echo 'Removing APT Pinning...'
 sudo rm /etc/apt/preferences.d/nativeapt

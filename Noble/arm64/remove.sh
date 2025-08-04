30d1dd293e3c0e15fd94c61eb6d1d8c52c1d7108
Alif <arfshl@protonmail.com>
2025-08-04 21:42:00 +0700

    Update remove.sh

--- Noble/arm64/remove.sh
+++ Noble/arm64/remove.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo 'Removing Additional Repository...'
-sudo apt remove debian-keyring -y
+sudo apt remove debian-archive-keyring -y
 sudo rm /etc/apt/sources.list.d/debian.list
 sudo rm /etc/apt/sources.list.d/mozilla.list
 sudo rm /etc/apt/keyrings/packages.mozilla.org.asc

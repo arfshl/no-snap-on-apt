5916ef8cbe7842cb40bf85d48a30acb9308642cf
Alif <arfshl@protonmail.com>
2025-08-11 21:28:56 +0700

    Delete Focal/amd64 directory

--- Focal/amd64/remove.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo 'Removing Additional Repository...'
-sudo apt remove linuxmint-keyring
-sudo rm /etc/apt/preferences.d/linuxmint.list
-echo 'Removing APT Pinning...'
-sudo rm /etc/apt/preferences.d/nativeapt
-sudo apt update
-echo 'Done.'

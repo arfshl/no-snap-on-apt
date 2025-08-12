34897aebf30a78f7e792627aabcbbdcd954dba9f
Alif <arfshl@protonmail.com>
2025-08-12 14:25:06 +0700

    Create setup.sh

---
+++ Jammy/armhf/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+echo 'Adding Debian Repository...'
+sudo wget -q http://ftp.us.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2023.3+deb12u2_all.deb
+sudo dpkg -i debian-archive-keyring_2023.3+deb12u2_all.deb
+sudo rm debian-archive-keyring_2023.3+deb12u2_all.deb
+sudo echo 'deb https://deb.debian.org/debian/ bullseye contrib main non-free
+deb https://security.debian.org/debian-security/ bullseye-security contrib main non-free' >> /etc/apt/sources.list.d/debian.list
+echo 'Adding Mozillateam PPA...'
+sudo add-apt-repository ppa:mozillateam/ppa -y
+echo 'Configuring APT Pinning...'
+sudo echo 'Package: *
+Pin: origin deb.debian.org
+Pin-Priority: 1
+
+Package: *
+Pin: origin security.debian.org
+Pin-Priority: 1
+
+Package: firefox
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1000
+
+Package: firefox-esr
+Pin: release o=LP-PPA-mozillateam
+Pin-Priority: 1000
+
+Package: chromium
+Pin: origin deb.debian.org
+Pin-Priority: 1000
+
+Package: chromium-common
+Pin: origin deb.debian.org
+Pin-Priority: 1000
+
+Package: chromium-sandbox
+Pin: origin deb.debian.org
+Pin-Priority: 1000
+
+Package: firefox
+Pin: release o=Ubuntu
+Pin-Priority: -1
+
+Package: chromium-browser
+Pin: release o=Ubuntu
+Pin-Priority: -1' >> /etc/apt/preferences.d/nativeapt
+sudo apt update
+echo 'Done'

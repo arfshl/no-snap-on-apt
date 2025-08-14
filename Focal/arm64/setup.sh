e0a70ce417ba9357af75e98a494dc0d2828900db
Alif <arfshl@protonmail.com>
2025-08-14 10:11:21 +0700

    Update setup.sh

--- Focal/arm64/setup.sh
+++ Focal/arm64/setup.sh
@@ -7,23 +7,19 @@ sudo echo 'deb https://archive.debian.org/debian/ buster contrib main non-free
 deb https://archive.debian.org/debian-security/ buster/updates contrib main non-free' >> /etc/apt/sources.list.d/debian.list
 echo 'Configuring APT Pinning...'
 sudo echo 'Package: *
-Pin: origin deb.debian.org
-Pin-Priority: 1
-
-Package: *
-Pin: origin security.debian.org
+Pin: origin archive.debian.org
 Pin-Priority: 1
 
 Package: chromium
-Pin: origin deb.debian.org
+Pin: origin archive.debian.org
 Pin-Priority: 1000
 
 Package: chromium-common
-Pin: origin deb.debian.org
+Pin: origin archive.debian.org
 Pin-Priority: 1000
 
 Package: chromium-sandbox
-Pin: origin deb.debian.org
+Pin: origin archive.debian.org
 Pin-Priority: 1000
 
 Package: chromium-browser

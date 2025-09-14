ddd7ee3ec0fea45ebb8ea742e08a9a62062f73d5
Alif <arfshl@protonmail.com>
2025-09-14 14:06:36 +0700

    Update setup.sh

--- Focal/arm64/setup.sh
+++ Focal/arm64/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 echo 'Adding Debian Repository...'
-sudo wget -q http://ftp.us.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2023.3+deb12u2_all.deb
+wget -q http://ftp.us.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2023.3+deb12u2_all.deb
 sudo dpkg -i debian-archive-keyring_2023.3+deb12u2_all.deb
-sudo rm debian-archive-keyring_2023.3+deb12u2_all.deb
-sudo echo 'deb https://archive.debian.org/debian/ buster contrib main non-free
+rm debian-archive-keyring_2023.3+deb12u2_all.deb
+sudo cat <<EOF > /etc/apt/preferences.d/nativeapt
+deb https://archive.debian.org/debian/ buster contrib main non-free
 deb https://archive.debian.org/debian-security/ buster/updates contrib main non-free' >> /etc/apt/sources.list.d/debian.list
 echo 'Configuring APT Pinning...'
 sudo echo 'Package: *
@@ -25,5 +26,7 @@ Pin-Priority: 1000
 Package: chromium-browser
 Pin: release o=Ubuntu
 Pin-Priority: -1' >> /etc/apt/preferences.d/nativeapt
+EOF
+
 sudo apt update
 echo 'Done'

fdcd01c5d2a86005584bcc5e7807d87fa72a3953
Alif <arfshl@protonmail.com>
2025-09-20 13:54:39 +0700

    Update setup.sh

--- Focal/arm64/setup.sh
+++ Focal/arm64/setup.sh
@@ -3,11 +3,11 @@ echo 'Adding Debian Repository...'
 wget -q http://ftp.us.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2023.3+deb12u2_all.deb
 sudo dpkg -i debian-archive-keyring_2023.3+deb12u2_all.deb
 rm debian-archive-keyring_2023.3+deb12u2_all.deb
-sudo cat <<EOF > /etc/apt/preferences.d/nativeapt
 deb https://archive.debian.org/debian/ buster contrib main non-free
 deb https://archive.debian.org/debian-security/ buster/updates contrib main non-free' >> /etc/apt/sources.list.d/debian.list
 echo 'Configuring APT Pinning...'
-sudo echo 'Package: *
+sudo cat <<EOF > /etc/apt/preferences.d/nativeapt
+Package: *
 Pin: origin archive.debian.org
 Pin-Priority: 1
 
